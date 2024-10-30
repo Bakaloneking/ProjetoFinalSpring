6d0555cdf043d46f0e9c019e99bc6911f59b2ce9
Carlos Nascimento <77477586+Bakaloneking@users.noreply.github.com>
2024-10-29 23:00:56 -0400

    Add files via upload

---
+++ main/java/edu/aranoua/aplicacao_spring_final/AplicacaoSpringFinalApplication.java
@@ -0,0 +1,13 @@
+package edu.aranoua.aplicacao_spring_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoSpringFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoSpringFinalApplication.class, args);
+	}
+
+}
---
+++ test/java/edu/aranoua/aplicacao_spring_final/AplicacaoSpringFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aranoua.aplicacao_spring_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoSpringFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
